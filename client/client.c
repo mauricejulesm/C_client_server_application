35a19a3e816940d01f6964c6e2532fafad2c9f19
Maurice Jules Mulisa <jmulisa15@alustudent.com>
2019-04-04 22:24:15 +0100

    arrange working directory

---
+++ client/client.c
@@ -0,0 +1,11 @@
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+int main(int argc, char* argv[]){
+return 0;
+}
